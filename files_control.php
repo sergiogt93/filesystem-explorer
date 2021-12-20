77d6aea10fe3dac679c714d8e7765d2f6722db43
sergiogt93 <sergio.ber0556@gmail.com>
2021-12-20 15:06:44 +0100

    Add information one file dynamic

--- files_control.php
+++ files_control.php
@@ -1,26 +1,5 @@
 <?php
 
-function getFileSize($path)
-{
-    $fileSize = filesize($path);
-    return ($fileSize < 1024)
-        ? $fileSize . "KB"
-        : round($fileSize / 1024, 2) . "MB";
-}
-
-function getFileInfo($path)
-{
-    $fileSize = filesize($path);
-    $pathInfo =  array(
-        "name" => pathinfo($path, PATHINFO_FILENAME),
-        "lastModified" => date("H, d M", strtotime(filemtime($path))),
-        "extension" => pathinfo($path, PATHINFO_EXTENSION),
-        "size" => getFileSize($fileSize)
-    );
-    return $pathInfo;
-}
-
-
 function listDirectory($dir){
     $list = scandir($dir);
 
